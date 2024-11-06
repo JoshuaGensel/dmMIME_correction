f29d089376c45e9ba9da1b8a628478c3af514eab
JoshuaGensel <josh.gnsl@gmail.com>
2024-11-06 11:01:54 +0100

    new correction for discrete stochastic simulations

--- src/new_correction.py
+++ src/new_correction.py
@@ -16,14 +16,14 @@ def get_pool_data(path : str, protein_concentrations : list):
     round_1_selected_frequencies = []
     round_2_selected_frequencies = []
 
-    ground_truth = np.loadtxt(path + f"target1_{protein_concentrations[0]}_target2_{protein_concentrations[0]}/ground_truth.csv", delimiter=",")
+    ground_truth = np.log(np.loadtxt(path + f"target1_{protein_concentrations[0]}_target2_{protein_concentrations[0]}/ground_truth.csv", delimiter=","))
 
     for protein_concentration_1 in protein_concentrations:
         for protein_concentration_2 in protein_concentrations:
             round_1_sequences.append(np.loadtxt(path + f"target1_{protein_concentration_1}_target2_{protein_concentration_2}/round_1/unique_sequences.csv", delimiter=","))
             round_2_sequences.append(np.loadtxt(path + f"target1_{protein_concentration_1}_target2_{protein_concentration_2}/round_2/unique_sequences.csv", delimiter=","))
-            round_1_sequence_effects.append(np.loadtxt(path + f"target1_{protein_concentration_1}_target2_{protein_concentration_2}/round_1/sequence_effects.csv", delimiter=","))
-            round_2_sequence_effects.append(np.loadtxt(path + f"target1_{protein_concentration_1}_target2_{protein_concentration_2}/round_2/sequence_effects.csv", delimiter=","))
+            round_1_sequence_effects.append(np.log(np.loadtxt(path + f"target1_{protein_concentration_1}_target2_{protein_concentration_2}/round_1/sequence_effects.csv", delimiter=",")))
+            round_2_sequence_effects.append(np.log(np.loadtxt(path + f"target1_{protein_concentration_1}_target2_{protein_concentration_2}/round_2/sequence_effects.csv", delimiter=",")))
             round_1_initial_frequencies.append(np.loadtxt(path + f"target1_{protein_concentration_1}_target2_{protein_concentration_2}/round_1/counts.csv", delimiter=","))
             round_2_initial_frequencies.append(np.loadtxt(path + f"target1_{protein_concentration_1}_target2_{protein_concentration_2}/round_2/counts.csv", delimiter=","))
             round_1_selected_frequencies.append(np.loadtxt(path + f"target1_{protein_concentration_1}_target2_{protein_concentration_2}/round_1/selected/counts.csv", delimiter=","))
@@ -31,10 +31,10 @@ def get_pool_data(path : str, protein_concentrations : list):
 
     return ground_truth, round_1_sequences, round_2_sequences, round_1_sequence_effects, round_2_sequence_effects, round_1_initial_frequencies, round_2_initial_frequencies, round_1_selected_frequencies, round_2_selected_frequencies
 
-def infer_logK_sequences(initial_frequencies: np.array, selected_frequencies: np.array, total_protein_concentration: float, c: float):
+def infer_logK_sequences(initial_frequencies: np.array, selected_frequencies: np.array, total_protein_concentration: float, c: float, number_sequences: int = 1):
 
     # compute free protein concentration
-    free_protein_concentration = total_protein_concentration - np.sum(selected_frequencies)
+    free_protein_concentration = total_protein_concentration - np.sum(selected_frequencies/number_sequences)
 
     # get indices where selected frequency or initial frequency - selected frequency is less than c
     prune_indices = np.where((selected_frequencies < c) | (initial_frequencies - selected_frequencies < c))
@@ -87,7 +87,7 @@ def infer_logK_mutations(logK_sequences : np.array, unique_sequences : np.array)
 
     return result.x
 
-def logK_inference(path : str, protein_concentrations : list, c : float):
+def logK_inference(path : str, protein_concentrations : list, c : float, number_sequences: int = 1):
 
     ground_truth, round_1_sequences, round_2_sequences, round_1_sequence_effects, round_2_sequence_effects, round_1_initial_frequencies, round_2_initial_frequencies, round_1_selected_frequencies, round_2_selected_frequencies = get_pool_data(path, protein_concentrations)
 
@@ -100,9 +100,9 @@ def logK_inference(path : str, protein_concentrations : list, c : float):
         protein_concentration_1 = protein_concentrations[i]
         for j in range(len(protein_concentrations)):
             protein_concentration_2 = protein_concentrations[j]
-            logK_sequences_r1.append(infer_logK_sequences(round_1_initial_frequencies[i*len(protein_concentrations) + j], round_1_selected_frequencies[i*len(protein_concentrations) + j], protein_concentration_1, c))
+            logK_sequences_r1.append(infer_logK_sequences(round_1_initial_frequencies[i*len(protein_concentrations) + j], round_1_selected_frequencies[i*len(protein_concentrations) + j], protein_concentration_1, c, number_sequences))
             logK_mutations_r1.append(infer_logK_mutations(logK_sequences_r1[i*len(protein_concentrations) + j], round_1_sequences[i*len(protein_concentrations) + j]))
-            logK_sequences_r2.append(infer_logK_sequences(round_2_initial_frequencies[i*len(protein_concentrations) + j], round_2_selected_frequencies[i*len(protein_concentrations) + j], protein_concentration_2, c))
+            logK_sequences_r2.append(infer_logK_sequences(round_2_initial_frequencies[i*len(protein_concentrations) + j], round_2_selected_frequencies[i*len(protein_concentrations) + j], protein_concentration_2, c, number_sequences))
             logK_mutations_r2.append(infer_logK_mutations(logK_sequences_r2[i*len(protein_concentrations) + j], round_2_sequences[i*len(protein_concentrations) + j]))
 
-    return logK_sequences_r1, logK_mutations_r1, logK_sequences_r2, logK_mutations_r2, np.log(ground_truth), np.log(round_1_sequence_effects), np.log(round_2_sequence_effects)
+    return logK_sequences_r1, logK_mutations_r1, logK_sequences_r2, logK_mutations_r2, ground_truth, round_1_sequence_effects, round_2_sequence_effects
